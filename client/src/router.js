37b4da03ff8e5eb38fee974d8729da1c21a42863
Anton Fresher <fresher@yandex-team.ru>
2019-05-18 21:15:45 +0500

    Фикс роутера

--- client/src/router.js
+++ client/src/router.js
@@ -10,13 +10,15 @@ class AppRouter extends React.Component {
     render() {
         return (
             <Router>
-                <Switch path="/" component={App}>
-                    <Redirect exact from="/" to="/list" />
-                    <Route path="/list" component={List} />
-                    {/* <Route path="/create" component={Create} />
-                    <Route path="/issue/:id" component={Issue} /> */
-                    /* <Route path="*" component={Error} status={404} /> */}
-                </Switch>
+                <App>
+                    <Switch>
+                        <Redirect exact from="/" to="/list" />
+                        <Route path="/list" component={List} />
+                        {/* <Route path="/create" component={Create} />
+                        <Route path="/issue/:id" component={Issue} /> */
+                        /* <Route path="*" component={Error} status={404} /> */}
+                    </Switch>
+                </App>
             </Router>
         );
     }
