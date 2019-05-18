0fc873dd9d516e8ef9bcea5776984908e3441471
Anton Fresher <fresher@yandex-team.ru>
2019-05-19 00:10:33 +0500

    Подцеплять issues

--- client/src/components/issues/issues.js
+++ client/src/components/issues/issues.js
@@ -8,10 +8,46 @@ import './issues.css';
 const b = b_.with('issues');
 
 class Issues extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loaded: false,
+            issues: [],
+            error: null
+        };
+    }
+
+    async componentDidMount() {
+        const res = await fetch('http://localhost:5000/issues');
+        const json = await res.json();
+
+        try {
+            this.setState({
+                loaded: true,
+                issues: json
+            });
+        } catch (error) {
+            this.setState({
+                loaded: true,
+                error
+            });
+        }
+    }
+
     render() {
+        const { issues } = this.state;
+
         return (
             <div className={b()}>
-                <Issue />
+                {issues.map(issue => (
+                    <Issue
+                        image={issue.image}
+                        name={issue.name}
+                        description={issue.description}
+                        votes={issue.votes}
+                    />
+                ))}
             </div>
         );
     }
