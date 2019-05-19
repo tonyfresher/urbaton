14a0960cda8f7c105d1e266a57b58899c377df90
Anton Fresher <fresher@yandex-team.ru>
2019-05-19 23:58:41 +0500

    Упростить сервер

--- server/index.py
+++ server/index.py
@@ -1,14 +1,132 @@
-import logging
+from uuid import uuid4
 
-from flask import Flask
+from flask import Flask, jsonify, request
+from storage.postgres import postgres
+from psycopg2 import InternalError, ProgrammingError
 
-from app import app as issues_app
+import geocoder
+
+from storage.postgres import postgres
 
 
 app = Flask(__name__)
-app.register_blueprint(issues_app)
+app.run(host='0.0.0.0', debug=True)
 
-if __name__ == '__main__':
-    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+def error(message):
+    return jsonify({
+        'status': 'error',
+        'message': message
+    })
 
-    app.run(host='0.0.0.0', debug=True)
+@app.after_request
+def cors(res):
+    headers = res.headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+
+    return res
+
+@app.route('/issues')
+def get_issues():
+    issues = postgres.get_issues()
+
+    return jsonify(issues)
+
+@app.route('/issues', methods=['POST'])
+def create_issue():
+    request_json = request.get_json()
+
+    if len(request_json) < 4:
+        return error('Not enough data for issue creation')
+    
+    name = request_json['name']
+    description = request_json['description']
+    image = request_json['image']
+    coordinates = request_json['coordinates']
+
+    lat = coordinates.get('lat')
+    lng = coordinates.get('lng')
+
+    if not(lat and lng):
+        return error('Coordinates are not valid')
+
+    if type(coordinates) != dict:
+        return error('Coordinates must be in json')
+
+    try:
+        geo_response = geocoder.yandex([lat, lng], method='reverse')
+    except ValueError as err:
+        return error(str(err))
+    
+    coordinates = {
+        'lat': geo_response.lat,
+        'lng': geo_response.lng,
+        'address': geo_response.address
+    }
+
+    postgres.create_issue(str(uuid4()), name, description, image, coordinates)
+
+    return jsonify('Issue successfully created')
+
+@app.route('/issues/<id>')
+def get_issue_by_id(id):
+    response = postgres.get_issue_by_id(id)
+
+    return jsonify(response) if response else error("Issue not found")
+
+@app.route('/issues/<id>', methods=['PUT'])
+def update_issue(id):
+    request_json = request.get_json()
+
+    name = request_json.get('name', '')
+    description = request_json.get('description', '')
+    image = request_json.get('image', '')
+    coordinates = request_json.get('coordinates', {})
+
+    try:
+        postgres.update_issue(id, name, description, image, coordinates)
+    except ProgrammingError as err:
+        return error(str(err))
+
+    return jsonify("Successfully updated")
+
+@app.route('/issues/<id>', methods=['DELETE'])
+def delete_issue(id):
+    postgres.delete_issue_by_id(id)
+
+    return jsonify('If id is right, then issue is deleted')
+
+@app.route('/issues/<id>/votes')
+def get_issue_votes(id):
+    response = postgres.get_issue_votes_by_id(id)
+
+    return response[0][0] if response else error('Issue not found')
+
+@app.route('/issues/<id>/votes', methods=['PUT'])
+def change_vote(id):
+    postgres.post_issue_votes_by_id(id)
+
+    return jsonify('If id is right, then vote is added')
+
+@app.route('/issues/<id>/projects', methods=['GET'])
+def get_project_by_id(id):
+    response = postgres.get_project(id)
+
+    return jsonify(response) if response else error("Project not found")
+
+@app.route('/issues/<id>/projects', methods=['POST'])
+def create_project(id):
+    request_json = request.get_json()
+
+    if len(request_json) < 3:
+        return error('Not enough data for issue creation')
+
+    name = request_json['name']
+    description = request_json['description']
+    cost = request_json['cost']
+
+    project_id = str(uuid4())
+    account_id = str(uuid4())
+    postgres.create_project(name, description, cost, account_id, project_id, id)
+    return jsonify("Successfully created")
